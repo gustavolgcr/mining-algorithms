ff4266911c201cb14c668a1bfa7af507fcedae03
Manoel Rui <manoelrui19@gmail.com>
2015-03-18 17:44:04 -0300

    Teste usando o CSVParser
    Criado um teste na função main para ler o primeiro atributo de todas as tuplas do dataset 'winequality-red.csv'

--- src/main/scala/org/algorithms/test.scala
+++ src/main/scala/org/algorithms/test.scala
@@ -4,5 +4,12 @@ package main.scala.org.algorithms
  * Created by gustavolgcr on 3/17/15.
  */
 object test {
-  def main(args: Array[String]) = println("Test!")
+  def main(args: Array[String]) {
+    var wines = CSVParser.readFile("datasets/winequality-red.csv")
+
+    var it = 0
+    for(wine <- wines) {
+      println(wine(0))
+    }
+  }
 }
