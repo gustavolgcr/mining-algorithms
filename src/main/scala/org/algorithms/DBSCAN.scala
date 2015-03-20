61684c120de11cbcea6cdaa501c694058762ea0f
Manoel Rui <manoelrui19@gmail.com>
2015-03-20 11:48:41 -0300

    Implementado método apply da classe DBSCAN
    Implementado método apply, correspondente ao método principal, da classe DBSCAN. Ainda, foi criado uma enumeração contendo os possíveis estados de um ponto qualquer no algoritmo.

--- src/main/scala/org/algorithms/DBSCAN.scala
+++ src/main/scala/org/algorithms/DBSCAN.scala
@@ -1,17 +1,41 @@
 package main.scala.org.algorithms
 
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+
 /**
  * Created by rui on 3/19/15.
  */
 object DBSCAN {
-  def apply(dataset: List[Array[Float]], eps: Float, minPts: Int):
-                    List[Array[Int]] = {
+  object PointState extends Enumeration {
+    type PointState = Value
+    val Visited, Unvisited, Noise = Value
+  }
 
-    return null
+  def apply(dataset: List[Array[Float]], eps: Float, minPts: Int):
+                    ListBuffer[ArrayBuffer[Int]] = {
+    var clusters = new ListBuffer[ArrayBuffer[Int]]()
+    var pointState = Array.fill(dataset.length)(PointState.Unvisited)
+
+    for(it <- 0 until dataset.length) {
+      var point = dataset(it)
+      if(pointState(it) == PointState.Unvisited) {
+        pointState(it) = PointState.Visited
+        var neighborPts = regionQuery(point, eps)
+        if(neighborPts.length < minPts) {
+          pointState(it) = PointState.Noise
+        } else {
+          var cluster = new ArrayBuffer[Int]()
+          clusters += cluster
+          expandCluster(point, neighborPts, cluster, eps, minPts)
+        }
+      }
+    }
+    
+    return clusters
   }
 
   def expandCluster(point: Array[Float], neighbotPts: List[Array[Float]],
-                    clusters: List[Array[Int]], eps: Float, minPts: Int) = {
+                    cluster: ArrayBuffer[Int], eps: Float, minPts: Int) = {
 
   }
 
