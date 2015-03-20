4f5bfd86940d4f6a5e03440267ab6da6940bcd8b
Manoel Rui <manoelrui19@gmail.com>
2015-03-20 10:35:00 -0300

    Criado declarações dos métodos da class DBSCAN

--- src/main/scala/org/algorithms/DBSCAN.scala
+++ src/main/scala/org/algorithms/DBSCAN.scala
@@ -4,5 +4,19 @@ package main.scala.org.algorithms
  * Created by rui on 3/19/15.
  */
 object DBSCAN {
+  def apply(dataset: List[Array[Float]], eps: Float, minPts: Int):
+                    List[Array[Int]] = {
 
+    return null
+  }
+
+  def expandCluster(point: Array[Float], neighbotPts: List[Array[Float]],
+                    clusters: List[Array[Int]], eps: Float, minPts: Int) = {
+
+  }
+
+  def regionQuery(point: Array[Float], eps: Float): List[Array[Float]] = {
+
+    return null
+  }
 }
