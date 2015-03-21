d9b3ffeac6a3e5cead005a9a89fc144b2528b41e
Manoel Rui <manoelrui19@gmail.com>
2015-03-21 16:36:21 -0300

    Consertado bug na classe DBSCAN
    Consertado um bug em que os elementos dentro de um cluster estavam se
    repetindo.

--- src/main/scala/org/algorithms/DBSCAN.scala
+++ src/main/scala/org/algorithms/DBSCAN.scala
@@ -7,6 +7,7 @@ import scala.collection.mutable.{ArrayBuffer, ListBuffer}
  */
 object DBSCAN {
   var pointState:Array[PointState.PointState] = null;
+  var pointInACluster:Array[Boolean] = null;
   var clusters: ListBuffer[ArrayBuffer[Int]] = null;
 
   object PointState extends Enumeration {
@@ -33,6 +34,7 @@ object DBSCAN {
     ListBuffer[ArrayBuffer[Int]] = {
     clusters = new ListBuffer[ArrayBuffer[Int]]()
     pointState = Array.fill(dataset.length)(PointState.Unvisited)
+    pointInACluster = Array.fill(dataset.length)(false)
 
     for(it <- 0 until dataset.length) {
       var pointIndex = it
@@ -52,26 +54,29 @@ object DBSCAN {
 
   def expandCluster(dataset: List[Array[Float]], pointIndex: Int, neighborPts:
     Set[Int], eps: Float, minPts: Int) = {
-    var neighborPtsCpy = neighborPts.toSet
+    var neighborPtsCpy = neighborPts.toList
     var cluster = new ArrayBuffer[Int]()
 
     cluster += pointIndex
     clusters += cluster
+    pointInACluster(pointIndex) = true
 
-    for(pointIndexInNeighborPts <- neighborPtsCpy) {
-      if(pointState(pointIndexInNeighborPts) == PointState.Unvisited) {
-        pointState(pointIndexInNeighborPts) = PointState.Visited
-        var neighborPts_ = regionQuery(dataset, pointIndexInNeighborPts, eps)
+    var i = 0
+    while(i < neighborPtsCpy.length) {
+      var tempLen = neighborPtsCpy.length
+      if(pointState(neighborPtsCpy(i)) == PointState.Unvisited) {
+        pointState(neighborPtsCpy(i)) = PointState.Visited
+        var neighborPts_ = regionQuery(dataset, neighborPtsCpy(i), eps)
         if(neighborPts_.size >= minPts) {
-          neighborPtsCpy = neighborPtsCpy.union(neighborPts_)
+          neighborPtsCpy = neighborPtsCpy ++ neighborPts_.diff(neighborPtsCpy.toSet).toList
         }
       }
 
-      for(clusterInClusters <- clusters) {
-        if(!clusterInClusters.contains(pointIndexInNeighborPts)) {
-          cluster += pointIndexInNeighborPts
-        }
+      if(!pointInACluster(neighborPtsCpy(i))) {
+        cluster += neighborPtsCpy(i)
       }
+
+      i = i + 1
     }
   }
 
