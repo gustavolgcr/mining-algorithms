817f1fc237a3bc15b2198a61f3bc947e68a66165
Manoel Rui <manoelrui19@gmail.com>
2015-03-23 09:20:45 -0300

    Modificado CSParser para ficar mais flexível
    Adicionado parametros para especificar a quantidade de linhas e colunas que serão excluídas pelo processo de leitrura. Adicionado parametro para especificar o caracter delimitador.

--- src/main/scala/org/algorithms/CSVParser.scala
+++ src/main/scala/org/algorithms/CSVParser.scala
@@ -4,9 +4,11 @@ package main.scala.org.algorithms
  * Created by lsbd on 18/03/15.
  */
 object CSVParser {
-  def readFile(fileName: String) : List[Array[String]] = {
+  def readFile(fileName: String, delimiter: Char = ';', dropNLines: Int = 1,
+    dropNColumn: Int = 1) : List[Array[String]] = {
     var src = scala.io.Source.fromFile(fileName);
-    var tempList = src.getLines().drop(1).map(_.split(";")).map(_.dropRight(1)).toList
+    var tempList = src.getLines().drop(dropNLines).map(_.split(delimiter)).
+      map(_.dropRight(dropNColumn)).toList
 
     src.close()
 
