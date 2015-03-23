a2e8b93be1689bc5dc1270c0ae41971098b8aaea
Manoel Rui <manoelrui19@gmail.com>
2015-03-23 08:58:18 -0300

    Removido comentários no CSVParser

--- src/main/scala/org/algorithms/CSVParser.scala
+++ src/main/scala/org/algorithms/CSVParser.scala
@@ -8,13 +8,6 @@ object CSVParser {
     var src = scala.io.Source.fromFile(fileName);
     var tempList = src.getLines().drop(1).map(_.split(";")).map(_.dropRight(1)).toList
 
-//    for(i <- 0 until tempList.length) {
-//      for(j <- 0 until tempList(i).length) {
-//        print(tempList(i)(j) + "; ")
-//      }
-//      println("")
-//    }
-
     src.close()
 
     return tempList
