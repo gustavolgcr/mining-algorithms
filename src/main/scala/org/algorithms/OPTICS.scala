0f2a84fa9b1df8e953936374b44d624b42890420
Luis Gustavo Coutinho do Rego <gustavolgcr@gmail.com>
2015-04-05 09:21:33 -0300

    Finalizando OPTICS.

--- src/main/scala/org/algorithms/OPTICS.scala
+++ src/main/scala/org/algorithms/OPTICS.scala
@@ -2,18 +2,21 @@ package main.scala.org.algorithms
 
 import scala.collection.mutable.{ArrayBuffer, ListBuffer, PriorityQueue, HashMap}
 
-class OPTICSPoint(_pointIndex: Int, _reachDistance: Float = -1, _coreDistance: Float = -1) {
+class OPTICSPoint(_pointIndex: Int, _reachDistance: Float = -1, _coreDistance: Float = -1,
+                  _dataPoint: Array[Float] = null, _isProcessed: Boolean = false, _clusterId: Int = -1) {
 
-  var pointIndex: Int = _pointIndex;
-  var reachDistance: Float = _reachDistance;
-  var coreDistance: Float = _coreDistance;
-  var clusterID: Int = -1;
+  var pointIndex: Int = _pointIndex
+  var reachDistance: Float = Float.MaxValue
+  var coreDistance: Float = _coreDistance
+  var dataPoint: Array[Float] = _dataPoint
+  var isProcessed: Boolean = _isProcessed
+  var clusterID: Int = _clusterId
 
 }
 
 class OPTICSPriorityQueue {
 
-  var seeds = PriorityQueue[OPTICSPoint]()(OPTICSPointOrdering);
+  var seeds = PriorityQueue[OPTICSPoint]()(OPTICSPointOrdering)
 
   def OPTICSPointOrdering = new Ordering[OPTICSPoint] {
     def compare(a : OPTICSPoint, b : OPTICSPoint) = b.reachDistance.compareTo(a.reachDistance)
@@ -23,10 +26,6 @@ class OPTICSPriorityQueue {
     this.seeds.find(x => Some(x.pointIndex) == Some(point)) match {
       case Some(p: OPTICSPoint) => {
         p.reachDistance = reachDistance
-
-        // See what happen with values between 0 and 1.
-        // Test if the priority of the queue change when we update the hash value.
-        // Creates a new queue and force the function to compare and update values.
         this.seeds = this.seeds.clone()
       }
       case None =>
@@ -36,48 +35,38 @@ class OPTICSPriorityQueue {
 
 object OPTICS {
 
-  var pointState:Array[PointState.PointState] = null;
-  var pQueue:OPTICSPriorityQueue = null;
-  var datasetPoints:Array[OPTICSPoint] = null;
+  var priorityQueue: OPTICSPriorityQueue = null;
 
-  object PointState extends Enumeration {
-    type PointState = Value
-    val Visited, Unvisited = Value
-  }
+  def apply(datasetPoints: Array[OPTICSPoint], eps: Float, minPts: Int) : ListBuffer[OPTICSPoint] = {
 
-  def apply(dataset: List[Array[Float]], eps: Float, minPts: Int) : ListBuffer[OPTICSPoint] = {
-
-    pointState = Array.fill(dataset.length)(PointState.Unvisited)
-
-    datasetPoints = new Array[OPTICSPoint](dataset.length)
-    pQueue = new OPTICSPriorityQueue
+    priorityQueue = new OPTICSPriorityQueue
     var resulList = new ListBuffer[OPTICSPoint]
 
-    // Initialize OPTICPoint's array
-    for(pointIndex <- 0 until datasetPoints.length) {
-      datasetPoints(pointIndex) = new OPTICSPoint(pointIndex, -1)
-
-    }
-
     for(point <- datasetPoints) {
-      if(pointState(point.pointIndex) == PointState.Unvisited) {
-        var neighborPts = regionQuery(dataset, point.pointIndex, eps)
 
-        pointState(point.pointIndex) = PointState.Visited
+      if(point.isProcessed == false) {
+
+        var neighborhoodPoints = regionQuery(datasetPoints, point, eps)
+
+        point.isProcessed = true
+        point.coreDistance = coreDistance(datasetPoints, neighborhoodPoints, point, eps, minPts)
         resulList += point
 
-        point.coreDistance = coreDistance(dataset, neighborPts, point.pointIndex, eps, minPts)
-        if (point.coreDistance != -1) {
-          update(dataset, neighborPts, point, pQueue, eps, minPts)
+        if (point.coreDistance < Float.MaxValue) {
 
-          while(!pQueue.seeds.isEmpty) {
-            var pointNeighbor = pQueue.seeds.dequeue()
-            var newNeighbors = regionQuery(dataset, pointNeighbor.pointIndex, eps)
-            pointNeighbor.coreDistance = coreDistance(dataset, newNeighbors, pointNeighbor.pointIndex, eps, minPts)
+          update(datasetPoints, neighborhoodPoints, point, priorityQueue, eps, minPts)
+
+          while(!priorityQueue.seeds.isEmpty) {
+
+            var pointNeighbor = priorityQueue.seeds.dequeue()
+
+            neighborhoodPoints = regionQuery(datasetPoints, pointNeighbor, eps)
+            pointNeighbor.coreDistance = coreDistance(datasetPoints, neighborhoodPoints, pointNeighbor, eps, minPts)
             resulList += pointNeighbor
 
             if(pointNeighbor.coreDistance != -1) {
-              update(dataset, newNeighbors, pointNeighbor, pQueue, eps,minPts)
+
+              update(datasetPoints, neighborhoodPoints, point, priorityQueue, eps, minPts)
             }
           }
         }
@@ -87,65 +76,75 @@ object OPTICS {
     return resulList
   }
 
-  def update(dataset: List[Array[Float]], neighbors: Set[Int], point: OPTICSPoint, Seeds: OPTICSPriorityQueue, eps: Float, minPoints: Int) : Unit = {
-    var coredist = point.coreDistance
-    var neighborList = neighbors.toList
+  def update(datasetPoints: Array[OPTICSPoint], neighborhoodPoints: Set[OPTICSPoint], point: OPTICSPoint, priorityQueue: OPTICSPriorityQueue, eps: Float, minPoints: Int) : Unit = {
 
-    for (it <- 0 until neighborList.length) {
-      if(pointState(neighborList(it)) == PointState.Unvisited) {
-        var newReachDist = math.max(coredist, Similarity.euclidean(dataset(point.pointIndex), dataset(neighborList(it))))
+    var coreDistance = point.coreDistance
+    var neighborhoodList = neighborhoodPoints.toList
 
-        if(datasetPoints(neighborList(it)).reachDistance == -1) {
-          datasetPoints(neighborList(it)).reachDistance = newReachDist
-          pQueue.seeds.enqueue(datasetPoints(neighborList(it)))
-        } else if (newReachDist < datasetPoints(neighborList(it)).reachDistance) {
-          datasetPoints(neighborList(it)).reachDistance = newReachDist
-          pQueue.updatePoint(neighborList(it), newReachDist)
+    for (neighborhoodIterator <- neighborhoodList) {
+      if(neighborhoodIterator.isProcessed == false) {
+
+        var newReachDistance = math.max(coreDistance, Similarity.euclidean(neighborhoodIterator.dataPoint, point.dataPoint))
+
+        if(neighborhoodIterator.reachDistance == -1) {
+
+          neighborhoodIterator.reachDistance = newReachDistance
+
+          priorityQueue.seeds.enqueue(neighborhoodIterator)
+
+        } else {
+
+          if (newReachDistance < neighborhoodIterator.reachDistance) {
+            neighborhoodIterator.reachDistance = newReachDistance
+            priorityQueue.updatePoint(neighborhoodIterator.pointIndex, newReachDistance)
+
+          }
         }
       }
     }
   }
 
-  //Double check this method
-  def coreDistance(dataset: List[Array[Float]], neighbors: Set[Int], pointIndex: Int, eps: Float, minPoints: Int) : Float = {
-    var distance = ListBuffer[Float]()
-    var neighborList = neighbors.toList
+  def coreDistance(datasetPoints: Array[OPTICSPoint], neighborhoodPoints: Set[OPTICSPoint], point: OPTICSPoint, eps: Float, minPoints: Int) : Float = {
 
-    if(neighborList.length < minPoints) {
+    def ordering = new Ordering[Float] {
+      def compare(a : Float, b : Float) = b.compareTo(a)
+    }
+
+    var distance = new PriorityQueue[Float]()(ordering)
+
+    var neighborhoodList = neighborhoodPoints.toList
+
+    if(neighborhoodList.length < minPoints) {
+
       return -1
 
     } else {
-      distance += Similarity.euclidean(dataset(pointIndex), dataset(neighborList(0)))
 
-      for(it <- 1 until neighborList.length) {
-
-        var lastIndex = distance.length
-
-        var tempEuclideanDistance = Similarity.euclidean(dataset(pointIndex), dataset(neighborList(it)))
-        distance += tempEuclideanDistance
-        while(lastIndex >= 0 && tempEuclideanDistance < distance(lastIndex)) {
-          distance(lastIndex + 1) = distance(lastIndex)
-          distance(lastIndex) = tempEuclideanDistance
-          lastIndex = lastIndex - 1
-        }
+      for(it <- 1 until neighborhoodList.length) {
+        distance.enqueue(Similarity.euclidean(point.dataPoint, neighborhoodList(it).dataPoint))
       }
 
-      return distance(minPoints-1)
+      for(it <- 0 until minPoints-2) {
+        distance.dequeue()
+      }
+
+      return distance.dequeue()
     }
   }
 
-  def regionQuery(dataset: List[Array[Float]], pointIndex: Int, eps: Float):
-  Set[Int] = {
-    var region = ListBuffer[Int]()
-    region += pointIndex
+  def regionQuery(datasetPoints: Array[OPTICSPoint], point: OPTICSPoint, eps: Float):Set[OPTICSPoint] = {
 
-    for(it <- 0 until dataset.length) {
-      if(Similarity.euclidean(dataset(pointIndex), dataset(it)) <= eps) {
-        region += it
+    var neighbors = ListBuffer[OPTICSPoint]()
+
+    for (currentPoint <- datasetPoints) {
+
+      if(Similarity.euclidean(point.dataPoint, currentPoint.dataPoint) <= eps) {
+        neighbors += currentPoint
       }
     }
 
-    return region.toSet
+    return neighbors.toSet
+
   }
 
   def extractDBSCAN(pQueue: ListBuffer[OPTICSPoint], ei: Float, minPoints: Int): Unit = {
@@ -153,20 +152,20 @@ object OPTICS {
 
     for(point <- pQueue) {
 
-
       if(point.reachDistance>ei){
+
         if(point.coreDistance<=ei){
+
           clusterID = clusterID+1
           point.clusterID = clusterID
+
         } else {
           point.clusterID = -1
         }
       } else {
         point.clusterID = clusterID
       }
-
     }
   }
-
 }
 
--- src/main/scala/org/algorithms/Tests.scala
+++ src/main/scala/org/algorithms/Tests.scala
@@ -2,10 +2,11 @@ package main.scala.org.algorithms
 
 import java.io.{File, PrintWriter}
 
-import scala.collection.mutable
+
 import scala.collection.mutable.{PriorityQueue, HashMap}
 
 object Tests {
+
   def testKMeans(): Unit = {
     val wines = CSVParser.readFile("datasets/winequality-red.csv")
 
@@ -43,78 +44,53 @@ object Tests {
 
   def testOPTICS: Unit = {
 
-    var eps = 0.4f
+    var eps = 0.1f
     var minPts = 4
 
-    var wines = CSVParser.readFile("datasets/complex9.txt", ',', 0, 1)
+    var datasetPoints:Array[OPTICSPoint] = null;
 
-//    for(wines <- wines){
-//      println(wines(0))
-//    }
-
+    var wines = CSVParser.readFile("datasets/diamonds9.txt", ',', 0, 1)
     println("Dataset read.")
 
     var normalizedWines = Normalization.featureScaling(wines)
-
     println("Dataset normalized.")
 
-    var clusters = OPTICS(normalizedWines, eps, minPts)
+    datasetPoints = new Array[OPTICSPoint](normalizedWines.length)
+
+    println("Populating array of Points.")
+    for(iterator <- 0 until datasetPoints.length) {
+
+      datasetPoints(iterator) = new OPTICSPoint(iterator, -1, -1, normalizedWines(iterator), false, -1)
+    }
+
+    var clusters = OPTICS(datasetPoints, eps, minPts)
 
     println("Clusters found.")
 
-    OPTICS.extractDBSCAN(clusters, 0.1f, 4)
+    println("Extracting clusters.")
+    OPTICS.extractDBSCAN(clusters, 0.035f, 4)
 
-    //Print on screen
+    var writer = new PrintWriter(new File("resultOptics.txt"))
+
     for(i <- 0 until clusters.length) {
-      println("pointIndex: " + clusters(i).pointIndex + " reachDistance: " + clusters(i).reachDistance + " coreDistance: " + clusters(i).coreDistance + " clusterID: " + clusters(i).clusterID)
-    }
 
-    var writer = new PrintWriter(new File("resultOptics"))
+      for(iterator <- clusters(i).dataPoint) {
+        writer.write(iterator + " ")
+      }
+      writer.write(clusters(i).clusterID + "\n")
 
-    //Print on file
-    for(i <- 0 until clusters.length) {
-      writer.write(clusters(i).pointIndex + " " + clusters(i).reachDistance + "\n")
     }
 
     writer.close()
 
-//    CSVParser.writeClustersFile("winequality-red-clustered.csv", wines, clusters)
-//
-//    println("File created with success")
-//    println("Numbe of clusters: " + clusters.length.toString)
-//
-//    // Print all clusters
-//    for(i <- 0 until clusters.length) {
-//      println("Cluster " + i)
-//      for (tupleindex <- clusters(i)) {
-//        print(tupleindex + " ")
-//      }
-//      println("\n")
-//    }
-
   }
 
-
   def main(args: Array[String]) {
-    //testKMeans
-//    testDBSCAN
 
-//    var p = new OPTICSPriorityQueue
-//
-//    p.seeds.enqueue(new OPTICSPoint(2, 0.0045f))
-//    p.seeds.enqueue(new OPTICSPoint(3, 0.01f))
-//    p.seeds.enqueue(new OPTICSPoint(4, 46))
-//    p.seeds.enqueue(new OPTICSPoint(5, 11))
-//    p.seeds.enqueue(new OPTICSPoint(7, 98))
-//
-//    p.updatePoint(2, 0.005f)
-//
-//    for(i <- 0 until p.seeds.length) {
-//      println(p.seeds.dequeue().reachDistance)
-//    }
-
-
+    testKMeans
+    testDBSCAN
     testOPTICS
 
   }
+
 }
