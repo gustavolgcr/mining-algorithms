ca505948609c34e4e6e14a3dd34ed20c780a7002
Roney <roneydecastro@gmail.com>
2015-04-22 15:21:43 -0300

    Passagem de argumentos por parâmetros.

--- src/main/scala/org/algorithms/Tests.scala
+++ src/main/scala/org/algorithms/Tests.scala
@@ -7,21 +7,20 @@ import scala.collection.mutable.{PriorityQueue, HashMap}
 
 object Tests {
 
-  def testKMeans(): Unit = {
-    val wines = CSVParser.readFile("datasets/winequality-red.csv")
+  def testKMeans(eps: Float, minPts: Int, dataset: String): Unit = {
+
+    val wines = CSVParser.readFile(dataset)
 
     val normalizedWines = Normalization.featureScaling(wines, 0, 1)
 
-    val kmeans : HashMap[Int, Int] = KMeans(normalizedWines, 3, 0.001f)
+    val kmeans : HashMap[Int, Int] = KMeans(normalizedWines, minPts, eps)
 
-    CSVParser.saveResult("result.csv", "datasets/winequality-red.csv", kmeans)
+    CSVParser.saveResult("result.csv", dataset, kmeans)
   }
 
-  def testDBSCAN: Unit = {
-    var eps = 0.02607f // melhores valores para o complex9
-    var minPts = 3
+  def testDBSCAN(eps: Float, minPts: Int, dataset: String): Unit = {
 
-    var wines = CSVParser.readFile("datasets/complex9.txt", ',', 0, 1)
+    var wines = CSVParser.readFile(dataset, ',', 0, 1)
 
     var normalizedWines = Normalization.featureScaling(wines)
 
@@ -40,14 +39,12 @@ object Tests {
     }
   }
 
-  def testOPTICS: Unit = {
-    var eps = 0.027f
-    var minPts = 3
+  def testOPTICS(eps: Float, minPts: Int, dataset: String): Unit = {
     var maxDistance = eps + 0.01f
 
     var datasetPoints:Array[OPTICSPoint] = null;
 
-    var wines = CSVParser.readFile("datasets/complex9.txt", ',', 0, 1)
+    var wines = CSVParser.readFile(dataset, ',', 0, 1)
     println("Dataset read.")
 
     var normalizedWines = Normalization.featureScaling(wines)
@@ -93,10 +90,52 @@ object Tests {
     writer.close()
   }
 
+
+
   def main(args: Array[String]) {
-    testKMeans
-    testDBSCAN
-    testOPTICS
+
+    if (args.length == 0) println(usage)
+    val arglist = args.toList
+    type OptionMap = Map[Symbol, Any]
+    def nextOption(map : OptionMap, list: List[String]) : OptionMap = {
+      def isSwitch(s : String) = (s(0) == '-')
+      list match {
+        case Nil => map
+        case "-algoritmo" :: value :: tail =>
+          nextOption(map ++ Map('algoritmo -> value), tail)
+        case "-dataset" :: value :: tail =>
+          nextOption(map ++ Map('dataset -> value), tail)
+        case "-minPts" :: value :: tail =>
+          nextOption(map ++ Map('minPts -> value), tail)
+        case "-eps" :: value :: tail =>
+          nextOption(map ++ Map('eps -> value), tail)
+        case string :: opt2 :: tail if isSwitch(opt2) =>
+          nextOption(map ++ Map('infile -> string), list.tail)
+        case string :: Nil =>  nextOption(map ++ Map('infile -> string), list.tail)
+        case option :: tail => println("Unknown option "+option)
+          exit(1)
+      }
+    }
+    val options = nextOption(Map(),arglist)
+
+    var minPts = options('minPts).toInt
+    var eps = options('eps).toFloat
+    var dataset = options('dataset).toString
+
+
+    options('algoritmo) match {
+
+      case "dbscan"  => println("dbscan")
+        testDBSCAN(eps, minPts, dataset)
+      case "optics"  => println("optics")
+        testOPTICS(eps, minPts, dataset)
+      case "kmeans"  => println("kmeans")
+        testKMeans(eps, minPts, dataset)
+      case _  => "Invalid month"
+    }
+
+
+
   }
 
 }
